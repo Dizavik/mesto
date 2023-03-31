b4192093dfbc5ccc4b2a2e056ba74f4ea4d5649e
Dizavik <120535629+Dizavik@users.noreply.github.com>
2023-04-01 01:15:16 +0500

    Версия 1
    Готовый проект

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+let popup = document.querySelector('.popup');
+let popupContainer = document.querySelector('.popup__container');
+let openPopupButton = document.querySelector('.profile__editbutton');
+let closePopupButton = document.querySelector('.popup__closeButton');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.field1');
+let aboutInput = document.querySelector('.field2');
+let submitPopupButton = document.querySelector('.popup__submit');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+  popupContainer.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  aboutInput.value = profileSubtitle.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  popupContainer.classList.remove('popup_opened');
+}
+
+function closePopupDocument(e) {
+  if (e.target === popup) {
+    popup.classList.remove('popup_opened');
+    popupContainer.classList.remove('popup_opened');
+  }
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = aboutInput.value;
+  closePopup(popup);
+}
+
+popupContainer.addEventListener('submit', handleFormSubmit);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+document.addEventListener('click', closePopupDocument);
